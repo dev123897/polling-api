fe0cc85d9d516361e3cc3efe8b47e47f620d1e68
Your Name <you@example.com>
2025-01-22 17:11:27 -0600

    Added Dockerfile Updated polling

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:lts-alpine as production
+WORKDIR /app
+COPY package*.json ./
+COPY .env ./
+RUN npm install --omit=dev
+COPY . .
+ENV NODE_ENV production
+ENV production 1
+EXPOSE 3002
+CMD ["node", "main.js"]
--- routes/ordering.js
+++ routes/ordering.js
@@ -24,23 +24,25 @@ setInterval(() => {
 }, oneWeek)
 
 // Create ordered resource
-function create(did, carrierId) {
-  db.query('SELECT did, expiration, status FROM vts.polling WHERE did = ?', [did])
+function create(freightId, freightCompanyId) {
+  console.log('creating resource')
+  db.query('SELECT freightId, expiration, status FROM vts.polling WHERE freightId = ?', [freightId])
     .then(([col, ]) => {
-      if (col.did === did && Date.now() < Date.parse(col.expiration) && col.status !== 'canceled') // if order has not expired and has not been canceled
-        db.query('INSERT INTO vts.dids (did, e911Enabled, enabled, carrierId, countryCode) VALUE (?,0,0,?,"+1")', [parseInt(did), parseInt(carrierId)])
-          .then(() => db.query('UPDATE vts.polling SET status = "complete" WHERE did = ?', [did]) )
+      if (col.freightId === freightId && Date.now() < Date.parse(col.expiration) && col.status !== 'canceled') // if order has not expired and has not been canceled
+        db.query('DELETE FROM orders WHERE freightId = ?', [freightId])
+        db.query('INSERT INTO freight (id, received, sent, freightCompanyId, warehouseCode) VALUE (?,0,0,?,"+1")', [parseInt(freightId), parseInt(freightCompanyId)])
+          .then(() => db.query('UPDATE vts.polling SET status = "complete" WHERE freightId = ?', [freightId]) )
           .catch(console.error)
     })
 }
 
-async function resourceExists (did) {
-  const dbRes = await db.query('SELECT TRUE FROM vts.dids WHERE did = ?', [parseInt(did)])
+async function resourceExists (freightId) {
+  const dbRes = await db.query('SELECT TRUE FROM vts.freight WHERE id = ?', [parseInt(freightId)])
   return dbRes.length
 }
 
 async function pollExists(req, res, next) {
-  const poll = await db.query('SELECT did, expiration, status FROM vts.polling WHERE id = ?', [req.params.id])
+  const poll = await db.query('SELECT freightId, expiration, status FROM vts.polling WHERE id = ?', [req.params.id])
 
   if (!poll.length) return res.status(404).json({ message: 'no such order' })
 
@@ -51,17 +53,18 @@ async function pollExists(req, res, next) {
 
 // Place order
 router.post('/order', wrap(async (req, res, ext) => {
-  if (!req.body?.did || !req.body?.carrierId) return res.sendStatus(400)
+  console.log('placing order')
+  if (!req.body.freightId || !req.body.freightCompanyId) return res.status(400).json({ message: 'missing parameter' })
 
-  if (await resourceExists(req.body.did)) return res.sendStatus(409) // Do not process request if resource already taken
+  if (await resourceExists(req.body.freightId)) return res.sendStatus(409) // Do not process request if resource already taken
 
   const id = randomHash(5)
   const { insertId } = await db.query(
-    'INSERT INTO vts.polling (id, status, expiration, did, carrierId) VALUE (?, "pending", DATE_ADD(NOW(), INTERVAL 1 WEEK), ?, ?)',
-    [id, req.body.did, req.body.carrierId]
+    'INSERT INTO polling (id, status, expiration, freightId, freightCompanyId) VALUE (?, "pending", DATE_ADD(NOW(), INTERVAL 1 WEEK), ?, ?)',
+    [id, req.body.freightId, req.body.freightCompanyId] // 256 max value for hash in table
   )
 
-  setTimeout(create, 250000, req.body.did, req.body.carrierId)
+  setTimeout(create, 120000, req.body.freightId, req.body.freightCompanyId) // 2min
 
   res.location('/v1/order/poll/' + id)
   res.sendStatus(202)
@@ -69,6 +72,7 @@ router.post('/order', wrap(async (req, res, ext) => {
 
 // Poll for current status of order
 router.get('/order/poll/:id', wrap(pollExists), wrap(async (req, res, ext) => {
+  console.log('polling...')
   if (req.poll.status !== 'complete') {
     const json = { status: req.poll.status }
 
@@ -91,10 +95,14 @@ router.delete('/order/cancel/:id', wrap(pollExists), wrap(async (req, res, ext) 
 
 // View fulfilled order
 router.get('/order/result/:id', wrap(pollExists), wrap(async (req, res, ext) => {
-  if ( req.poll.status === 'complete' && !(await resourceExists(req.poll.did)) )
+  console.log('order complete')
+  if ( req.poll.status === 'complete' && !(await resourceExists(req.poll.freightId)) )
     return res.status(500).json({ message: 'Error: completed order is missing' })
 
-  res.status(200).json({ message: 'order fulfilled' })
+  res.status(200).json({
+    status: req.poll.status,
+    message: 'order fulfilled'
+  })
 }))
 
 module.exports = router
